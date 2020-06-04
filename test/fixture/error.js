69a002422b8481debf90ca9c4fde44e14843c8f9
oohira <oohira@ekzm.co.jp>
2020-06-04 15:25:51 +0900

    cp

---
+++ test/fixture/error.js
@@ -0,0 +1,4 @@
+require('./message');
+
+/* eslint-disable no-undef */
+intentionally_undefined();
---
+++ test/ts/dep.ts
@@ -0,0 +1,5 @@
+
+export const fn = (x: number) => {  
+  console.log('function from dep module here')
+}
+
